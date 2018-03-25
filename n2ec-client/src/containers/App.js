0d92a5f9080edc9fc3d9c01ebd69a0d840b4b56c
Merril Jeffs <merril@merrils-mbp.lan>
2018-03-25 17:59:36 -0600

    added material-ui package and edited react file structure

---
+++ n2ec-client/src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './App.css'
+
+const App = () => (
+    <MuiThemeProvider>
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">N2 Electrical Contractors, LLC</h1>
+            </header>
+        </div>
+    </MuiThemeProvider>
+)
+
+export default App
