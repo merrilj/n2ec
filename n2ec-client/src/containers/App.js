2577c07c5dc6d97c69e1c11491fb3af34c9ecae7
Merril Jeffs <merril@merrils-mbp.lan>
2018-03-29 18:35:03 -0600

    added initial topbar and title

--- n2ec-client/src/containers/App.js
+++ n2ec-client/src/containers/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import TopBar from '../components/TopBar'
 import './App.css'
 
 const App = () => (
     <MuiThemeProvider>
         <div className="App">
-            <header className="App-header">
-                <h1 className="App-title">N2 Electrical Contractors, LLC</h1>
-            </header>
+            <TopBar />
         </div>
     </MuiThemeProvider>
 )
 
 export default App
+
+// on contact page if you're interested in working with us send us a message and submit a resume
+// 
+
+
+
+// toll free number 885-ask-n2ec
