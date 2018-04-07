829c875fde75c240b9d4702cb7f79575f23b659a
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2018-04-07 16:43:22 -0600

    added main page components

--- n2ec-client/src/components/Main.js
+++ n2ec-client/src/components/Main.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const Main = () => (
     <div>
         <Logo />
+        <Card style={{width: '90%', margin: '0 auto', marginBottom: '2em'}}>
+    <CardHeader
+      title="URL Avatar"
+      subtitle="Subtitle"
+      avatar="images/jsa-128.jpg"
+    />
+    <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+    <CardActions>
+      {/* <FlatButton label="Action1" /> */}
+      {/* <FlatButton label="Action2" /> */}
+    </CardActions>
+  </Card>
     </div>
 )
 
